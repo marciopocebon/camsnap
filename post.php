80add2cc34c80a88982008a9f387b506eaaaf665
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-01 03:34:12 +0300

    Update post.php

--- post.php
+++ post.php
@@ -2,11 +2,11 @@
 $date = date('dMYHis');
 $imageData=$_POST['cat'];
 if (!empty($_POST['cat'])) {
-error_log("Received" . "\r\n", 3, "Log.log");
+error_log("Received" . "\r\n", 3, "log.log");
 }
 $filteredData=substr($imageData, strpos($imageData, ",")+1);
 $unencodedData=base64_decode($filteredData);
-$fp = fopen( '~/camsnap/images/cam'.$date.'.png', 'wb' );
+$fp = fopen( 'cam'.$date.'.png', 'wb' );
 fwrite( $fp, $unencodedData);
 fclose( $fp );
 exit();
